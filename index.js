dcb3a4b279a433ed1fdcbac8ef4c816f0d5bbddd
thatsnotrlght <jartiste23@gmail.com>
2025-09-25 16:50:24 -0400

    Add 3x3 grid and image tiling logic

--- index.js
+++ index.js
@@ -8,12 +8,40 @@ fileInput.addEventListener('change', function() {
         reader.onload = function(e) {
             canvas.innerHTML = "";
 
-            let img = document.createElement("img");
-            img.src = e.target.result;
-
+            // let img = document.createElement("img");
+            // img.src = e.target.result;
+            createGrid(e.target.result);
             canvas.appendChild(img);
         };
 
         reader.readAsDataURL(file);
     }
 })
+
+
+function createGrid(imgSrc) {
+    canvas.innterHTML = "";
+
+    let tileSize = 100;
+    let fullSize = 300;
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (row === 2 && col === 2) {
+                let emptyTile = document.createElement("div");
+                emptyTile.classList.add("tile", "empty");
+                continue;
+            }
+
+            let tile = document.createElement("div");
+            tile.classList.add("tile")
+
+            tile.style.backgroundImage = `url(${imgSrc})`;
+            tile.style.backgroundPosition = `-${col * tileSize}px -${row * tileSize}px`;
+
+            canvas.appendChild(tile);
+
+        }
+
+    }
+}
